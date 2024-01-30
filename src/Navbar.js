b43cc94766e9b33df05b7170e2c77c107ef5c57a
SumitGupta24 <gupta2411sumit@gmail.com>
2024-01-30 23:03:29 +0530

    third commit to fix links

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} mb-2`}>
       <div className="container-fluid">
 
-    <a className="navbar-brand" href="/">{props.title}</a>
+    <a className="navbar-brand" href="#">{props.title}</a>
 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -18,11 +18,11 @@ export default function Navbar(props) {
        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
        <li className="nav-item">
-          <a className="nav-link" href="/">{props.about}</a>
+          <a className="nav-link" href="#">{props.about}</a>
       </li>
 
         <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="/">{props.name}</a>
+          <a className="nav-link" aria-current="page" href="#">{props.name}</a>
         </li>
 
       </ul>
